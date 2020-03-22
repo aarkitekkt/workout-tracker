c955e4affffa1c563fd4dd3386d9f0b29d5cbda7
aarkitekkt <johnson.brycej@gmail.com>
2020-03-21 20:48:28 -0600

    install npm packages and setup server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`Workout Tracker is running on port ${PORT}!`);
+});
